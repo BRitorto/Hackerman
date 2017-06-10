35a5543c1bc630598789ff8671699c2bcd0bb75d
Bianca <britorto@itba.edu.ar>
2017-06-10 15:07:38 -0300

    a ver si esto funca de una puta vez

---
+++ desktop/src/com/poo/hackerman/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.poo.hackerman.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.poo.hackerman.view.Application;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Application(), config);
+	}
+}
