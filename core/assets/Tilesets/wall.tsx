39d590244b78c4fc7fbdbc131243ff57023d777a
cderienzo <cderienzo@itba.edu.ar>
2017-06-13 17:42:11 -0300

    Tiraba error con <TextureRegion>

--- core/assets/Tilesets/wall.tsx
+++
@@ -1,9 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset name="ball" tilewidth="32" tileheight="32" tilecount="1" columns="1">
- <image source="../ball.png" width="32" height="32"/>
- <tile id="0">
-  <objectgroup draworder="index">
-   <object id="1" x="0" y="-3" width="30" height="34"/>
-  </objectgroup>
- </tile>
-</tileset>
