a05d69d0d163cab2aea9c5a43bcc355f4a42b3b0
cderienzo <cderienzo@itba.edu.ar>
2017-06-21 21:12:50 -0300

    camera

--- core/src/com/poo/hackerman/view/GameScreen.java
+++ core/src/com/poo/hackerman/view/GameScreen.java
@@ -115,6 +115,7 @@ public class GameScreen extends ScreenAdapter {
 
     private void createEnemies(List<EnemyCharacter> enemiesO) {
         enemies = new UIEntity[enemiesO.size()];
+        //A ver
         cameras = new UIStaticEntity[enemiesO.size()];
         for(int i = 0; i < enemiesO.size() ; i++) {
             if(enemiesO.get(i).getClass().equals(CameraGuard.class)) {
