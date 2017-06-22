6eef5763149251865e20facb715a5df49a013914
Frannx1 <fsanguineti@itba.edu.ar>
2017-06-21 21:22:08 -0300

    ahora anda

--- core/src/com/poo/hackerman/model/gameWorld/Level.java
+++ core/src/com/poo/hackerman/model/gameWorld/Level.java
@@ -69,12 +69,12 @@ public class Level {
                     Direction direction = new Direction(0);
                     obstacleList.add(new Obstacle(position, direction, Obstacle.obstacleType.WALL));
                 }
-                if(cell.equals("DOOR")) {
+                else if(cell.equals("DOOR")) {
                     String[] properties = doors[doorIndex++].split(",");
                     Direction direction = new Direction(Integer.valueOf(properties[0]));
                     door = new Door(position, direction);
                 }
-                if(cell.equals("GUARD")) {
+                else if(cell.equals("GUARD")) {
                     String[] properties = guards[guardIndex++].split(",");
                     Direction direction = new Direction(Integer.valueOf(properties[0]));
                     int velocity = Integer.valueOf(properties[1]);
@@ -89,7 +89,7 @@ public class Level {
                         enemyList.add(new Guard(position, direction, velocity, range));
                     }
                 }
-                if(cell.equals("CAMERAGUARD")) {
+                else if(cell.equals("CAMERAGUARD")) {
                     String[] properties = cameraguards[cameraIndex++].split(",");
                     Direction direction = new Direction(Integer.valueOf(properties[0]));
                     Integer range = Integer.valueOf(properties[1]);
@@ -103,20 +103,20 @@ public class Level {
                         enemyList.add(new CameraGuard(position, direction, range));
                     }
                 }
-                if(cell.equals("PLAYER")) {
+                else if(cell.equals("PLAYER")) {
                     String[] properties = hackers[0].split(",");
                     Direction direction = new Direction(Integer.valueOf(properties[0]));
                     Integer velocity = Integer.valueOf(properties[1]);
                     hacker = new PlayerCharacter(position, direction, 10);   //cambiar velocidad
                 }
-                if(cell.equals("COMPUTER")) {
+                else if(cell.equals("COMPUTER")) {
                     String[] properties = computers[computerIndex].split(",");
                     Direction direction = new Direction(Integer.valueOf(properties[0]));
                     int consecutiveHacks = Integer.valueOf(properties[1]);
                     computerList.add(new Computer(position, direction, consecutiveHacks));
                 }
 
-                if(cell.equals("DESK")) {
+                else if(cell.equals("DESK")) {
                     String[] properties = desks[deskIndex].split(",");
                     Direction direction = new Direction(Integer.valueOf(properties[0]));
                     obstacleList.add(new Obstacle(position, direction, Obstacle.obstacleType.DESK));
